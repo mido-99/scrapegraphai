869c1761c40b80d7b5bdc99c2002e1328820708e
mido <midohany910@gmail.com>
2026-03-08 08:22:21 +0200

    Pydantic model for a fixed output schema

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ API_KEY = os.getenv('GEMINI_API')
 if not API_KEY:
     raise ValueError('Ensure GEMINI_API is set in your environment variables!')
 
+
 # Define the configuration for the scraping pipeline
 graph_config = {
    "llm": {
@@ -18,15 +19,37 @@ graph_config = {
    "headless": False,
 }
 
+# Define output schema
+from pydantic import BaseModel, Field
+
+class ArticleData(BaseModel):
+    content: str = Field(description="Main quote content")
+    author: str = Field(description="The author's name")
+    author_url: str = Field(description="The author's page URL full URL, homepage is 'https://quotes.toscrape.com'")
+    description: str = Field(description="Brief explanation of the quote's meaning")
+    tags: list = Field(description="Quote tags")
+
+# User prompt
+prompt="""
+Extract useful information about each quote in the page, including: 
+- content
+- author
+- author URL
+- tags
+- brief explanation
+"""
+
 # Create the SmartScraperGraph instance
 smart_scraper_graph = SmartScraperGraph(
-    prompt="Extract useful information from the webpage, including a description of what the company does, founders and social media links",
-    source="https://scrapegraphai.com/",
-    config=graph_config
+    prompt=prompt,
+    source="https://quotes.toscrape.com/",
+    config=graph_config,
+    schema=ArticleData
 )
 
 # Run the pipeline
 result = smart_scraper_graph.run()
 
 import json
-print(json.dumps(result, indent=4))
+with open('data.json', 'w') as file:
+    json.dump(result, file, indent=2)
